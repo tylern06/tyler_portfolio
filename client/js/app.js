f5fb918847de7fee38fbad6bcb72d1a2f20f218c
Tyler Nguyen <tynguyen06@gmail.com>
2017-02-24 01:33:51 -0800

    added angular

---
+++ client/js/app.js
@@ -0,0 +1,31 @@
+var myAppModule = angular.module('myApp', ['ngRoute','ngAnimate']);
+myAppModule.config(function ($routeProvider) {
+	$routeProvider
+	.when('/home',{
+		templateUrl: 'partials/home.html'
+	})
+	.when('/about',{
+		templateUrl:'partials/about.html'
+	})
+  .when('/project/1',{
+    templateUrl:'partials/project-1.html'
+  })
+  .when('/project/2',{
+    templateUrl:'partials/project-2.html'
+  })
+  .when('/project/3',{
+    templateUrl:'partials/project-3.html'
+  })
+  .when('/project/4',{
+    templateUrl:'partials/project-4.html'
+  })
+  .when('/project/5',{
+    templateUrl:'partials/project-5.html'
+  })
+  .when('/project/6',{
+    templateUrl:'partials/project-6.html'
+  })
+	.otherwise({
+		redirectTo: '/home'
+	});
+});
